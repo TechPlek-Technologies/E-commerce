7d6890fffabd1931d21b514cd9d46476b0cf9550
Deepak Sharma <d33paksharma.39@gmail.com>
2024-06-06 16:02:58 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const connection = require("./utils/connection");
 const app = express();
 app.use(express.json());
 const corsOptions = {
-  origin: ['http://localhost:8081',"http://localhost:3000"], // Replace with your client URL
+  origin: ["https://e-commerce-1-0ff7.onrender.com","https://e-commerce-yqol.onrender.com"], // Replace with your client URL
   credentials: true, // This allows the server to accept cookies and other credentials
 };
 
